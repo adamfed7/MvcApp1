e1a2f668b8e7e3253783fd01f6203e0508dc5371
Adam <Adam@GAMING>
2021-11-02 22:07:08 +0100

    Add project files.

---
+++ MvcApp1/Models/FormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp1.Models
+{
+    public class FormModel
+    {
+        public string Name { get; set; }
+        public DateTime DateBirth { get; set; }
+
+    }
+    
+}
+
